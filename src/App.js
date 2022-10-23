12141551140cc81eee0fc7eedbba49d5d9006300
Sofia Lumme <sofia.lumme@gmail.com>
2022-10-24 02:44:11 +0300

    tehtävä valmis

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import './App.css';
+import React, { useState } from 'react';
+import Home from './components/Home';
 import Todolist from './components/Todolist';
+import Tabs from'@mui/material/Tabs';
+import Tab from'@mui/material/Tab';
 
 function App() {
+  const [value, setValue] = useState('home');
+
+  const handleChange = (event, value) => {
+    setValue(value);
+  }
+
   return (
     <div className="App">
-      <Todolist />
+      <Tabs value={value} onChange={handleChange}>
+        <Tab value="home" label="Home" />
+        <Tab value="todos" label="Todos" />
+      </Tabs>
+      {value === 'home' && <Home />}
+      {value === 'todos' && <Todolist />}
     </div>
   );
 }
